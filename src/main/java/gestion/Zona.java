c16b1f40e4b4de62b570b591b362e1bf5c31beba
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2022-09-26 10:29:01 -0500

    Update Zona.java

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -5,47 +5,32 @@ import zooAnimales.Animal;
 public class Zona {
 	private String nombre;
 	private Zoologico zoo;
-	private static ArrayList<Animal> animales = new ArrayList<>();
+	private ArrayList<Animal> animales = new ArrayList<>();
 
-	public Zona(String nombre, Zoologico zoo) {
-		this.nombre = nombre;
-		this.zoo = zoo;
+public Zona(String nombre, Zoologico zoo) {
+	this.nombre=nombre;
+	this.zoo=zoo;
+}
+public Zona() {
+	this(null, null);
+}
+public void agregarAnimales(Animal animal) {
+	animales.add(animal);
+}
+public int cantidadAnimales() {
+	return animales.size();
 	}
-	
-	public Zona() {
-		this(null,null);
-	}
+public Zoologico getZoo() {
+	return zoo;
+}
+public void setZoo(Zoologico zoo) {
+	this.zoo=zoo;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre=nombre;
+}
 
-	public static void agregarAnimales(Animal nuevoAnimal) {
-		animales.add(nuevoAnimal);
-	}
-	
-	public int cantidadAnimales() { /*Talvez es poner los get y set de primeros antes que este*/
-		return animales.size();
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public Zoologico getZoo() {
-		return zoo;
-	}
-
-	public void setZoo(Zoologico zoo) {
-		this.zoo = zoo;
-	}
-
-	public static ArrayList<Animal> getAnimales() {
-		return animales;
-	}
-
-	public static void setAnimales(ArrayList<Animal> animales) {
-		Zona.animales = animales;
-	}
-	
 }
